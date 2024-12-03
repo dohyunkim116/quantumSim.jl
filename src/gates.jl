5ad90e8c6254ac63d508ad9ea587ef9e2cc8275e
Do Hyun Kim <dkim116@ucla.edu>
2024-12-02 18:15:25 -0800

    Initial prototype

---
+++ src/gates.jl
@@ -0,0 +1,64 @@
+function h!(i::Int, s::State{T}) where T
+    @assert i <= s.n && i > 0 "Invalid qubit index"
+    stemp = State(s.n);
+    mask_amp = s.amps .!= zero(eltype(s.amps))
+    indices_amp = findall(mask_amp)
+    for l in indices_amp, k in [0, 1]
+        temp = similar(s.kets[l]);
+        temp .= s.kets[l];
+        temp[i] = k;
+        idx = quantumSim.bitarray_to_index(temp)    
+        stemp.amps[idx] += s.amps[l] * (-one(T))^(k * s.kets[l][i]) / sqrt(2)
+    end
+    fillket!(stemp)
+    s.amps .= stemp.amps
+    s.kets .= stemp.kets
+end
+
+function fillket!(s)
+    mask_amp = s.amps .!= zero(eltype(s.amps))
+    indices_amp = findall(mask_amp)
+    s.kets[indices_amp] .= quantumSim.index_to_bitarray.(indices_amp, Ref(s.n))
+end
+
+function _x!(i::Int, ket::BitArray)
+    ket[i] = !ket[i]
+end
+
+function x!(i::Int, s::State)
+    @assert i <= s.n && i > 0
+    mask = s.amps .!= zero(eltype(s.amps))
+    _x!.(Ref(i), s.kets[mask]);
+end
+
+function t!(i::Int, s::State)
+    @assert i <= s.n && i > 0
+    mask_amp = s.amps .!= zero(eltype(s.amps))
+    mask_assn = [isassigned(s.kets, i) for i in eachindex(s.kets)]
+    mask_ket = [b == 1 ? s.kets[l][i] : false for (l, b) in enumerate(mask_assn)]
+    mask = mask_amp .& mask_ket
+    s.amps[mask] .*= exp(1.0im * π / 4)
+end
+
+function tdg!(i::Int, s::State)
+    @assert i <= s.n && i > 0
+    mask_amp = s.amps .!= zero(eltype(s.amps))
+    mask_assn = [isassigned(s.kets, i) for i in eachindex(s.kets)]
+    mask_ket = [b == 1 ? s.kets[l][i] : false for (l, b) in enumerate(mask_assn)]
+    mask = mask_amp .& mask_ket
+    s.amps[mask] .*= exp(-1.0im * π / 4)
+end
+
+function cx!(i::Int, j::Int, s::State)
+    @assert i <= s.n && i > 0
+    @assert j <= s.n && j > 0
+    @assert i != j
+    mask_amp = s.amps .!= zero(eltype(s.amps))
+    mask_assn = [isassigned(s.kets, i) for i in eachindex(s.kets)]
+    mask_ket = [b == 1 ? s.kets[l][i] : false for (l, b) in enumerate(mask_assn)]
+    mask = mask_amp .& mask_ket
+    for ket in s.kets[mask]
+        ket[j] = !ket[j]
+    end
+end
+
--- src/quantumSim.jl
+++ src/quantumSim.jl
@@ -1,5 +1,88 @@
 module quantumSim
 
-# Write your package code here.
+using LinearAlgebra
+export QSim, WeightedKet, State, x!, t!, tdg!, cx!, h!
+
+
+struct QSim{T <: Complex}
+	n::Int # number of qubits
+	m::Int # number of classical bits
+	ops::Array{Array{T, 2}, 1}
+	measure_low::Int
+	measure_high::Int
+end
+
+function QSim(
+	n::Int,
+	m::Int,
+	measure_low::Int = 1,
+	measure_high::Int = n,
+)
+	ops = Array{Complex, 2}[]
+	return QSim(n, m, ops, measure_low, measure_high)
+end
+
+struct WeightedKet{T <: Complex}
+	amp::Vector{T} # amplitude
+	ket::BitArray # |x>
+end
+
+function WeightedKet(
+	n::Int,
+	amp::Complex = 1.0 + 0.0im,
+	ket::BitArray = falses(n),
+)
+	@assert length(ket) == n
+	return WeightedKet([amp], ket)
+end
+
+function bitarray_to_index(bits::BitArray)
+	return foldl((acc, b) -> acc * 2 + b, bits, init = 0) + 1
+end
+
+function index_to_bitarray(value::Int, n::Int)
+    @assert value >= 1 && value <= 2^n
+	bits = reverse(digits(value - 1, base = 2))
+	return BitArray(vcat(zeros(Int, n - length(bits)), bits))
+end
+
+struct State{T <: Complex, TT <: BitArray}
+	n::Int
+	amps::Vector{T}
+	kets::Vector{TT}
+end
+
+function State(n::Int, wks::Vector{WeightedKet{T}}=WeightedKet{ComplexF64}[]) where T <: Complex
+    if isempty(wks)
+        return State(n, zeros(T, 2^n), Vector{BitArray}(undef, 2^n))
+    end
+	@assert all(length(wk.ket) == n for wk in wks)
+	"All WeightedKet objects must have the same n number of qubits."
+	amps = zeros(T, 2^n)
+	kets = Vector{BitArray}(undef, 2^n)
+	for wk in wks
+		idx = bitarray_to_index(wk.ket)
+		amps[idx] = wk.amp[1]
+		kets[idx] = wk.ket
+	end
+	return State(n, amps, kets)
+end
+
+function state(wks::Vector{WeightedKet{T}}) where T <: Complex
+	if isempty(wks)
+		throw(ArgumentError("Input vector wks cannot be empty."))
+	end
+	if any(length(wk.ket) != length(wks[1].ket) for wk in wks)
+		throw(ArgumentError("All WeightedKet objects must have the same number of qubits."))
+	end
+	state = zeros(T, 2^length(wks[1].ket))
+	for wk in wks
+		idx = bitarray_to_index(wk.ket)
+		state[idx] = wk.amp[1]
+	end
+	return state
+end
+
+include("gates.jl")
 
 end
